3b0a80cdb9679d7637c80141d495634875d29edd
mugisha-blaine <n.blaine@alustudent.com>
2023-09-13 08:17:56 +0200

    minoperations

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def minOperations(n):
+    """
+    returns the fewest number of operation need to result in exactly
+    n H characters in the file
+    """
+    operations = 0
+    b = 2
+    while n > 1:
+        while n % b == 0:
+            operations += b
+            n = n / b
+        b += 1
+    return operations
